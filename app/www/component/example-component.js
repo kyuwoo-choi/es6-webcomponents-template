0205b1b3caeca146c8bb9a8860b1fb70e6358079
ChoiKyu Woo <apple@paio.co.kr>
2015-05-25 04:27:16 +0900

    refactor project layout and gulp file.

--- app/www/component/example-component.js
+++ app/www/component/example-component.js
@@ -7,10 +7,24 @@ class ExampleComponent extends HTMLElement {
     }
 
     createdCallback () {
+        var findTemplate = function (dom, selector) {
+            var foundTemplate = dom.querySelectorAll(selector);
+            if (foundTemplate.length > 0) {
+                return foundTemplate[ 0 ];
+            }
+            var links = dom.querySelectorAll('link[rel="import"]');
+            for (var i = 0; i < links.length; i += 1) {
+                foundTemplate = findTemplate(links[ i ].import, selector);
+                if (foundTemplate !== null) {
+                    return foundTemplate;
+                }
+            }
+            return null;
+        };
         var shadow = this.createShadowRoot();
 
         var template = this.ownerDocument.querySelector('#example-component')
-            || this.ownerDocument.querySelector('link[rel="import"]').import.querySelector('#example-component');
+            || findTemplate(this.ownerDocument.querySelector('link[rel="import"]').import, '#example-component');
         var templateInstance = this.ownerDocument.importNode(template.content, true);
 
         shadow.appendChild(templateInstance);
